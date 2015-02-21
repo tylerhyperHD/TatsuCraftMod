bd2040691b1674df3f3ea088eab515c8a857ae5a
Robinson Gallego <robinson.leal7@gmail.com>
2015-02-21 17:37:56 -0500

    Update TFM_Util.java

--- src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
@@ -66,7 +66,8 @@ public class TFM_Util
 {
     private static final Map<String, Integer> ejectTracker = new HashMap<String, Integer>();
     public static final Map<String, EntityType> mobtypes = new HashMap<String, EntityType>();
-    public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "DarthSalamon", "AcidicCyanide", "wild1145", "WickedGamingUK");
+    public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "DarthSalamon", "AcidicCyanide", "wild1145", "WickedGamingUK", "xDestroyer217
+    );
     private static final Random RANDOM = new Random();
     public static String DATE_STORAGE_FORMAT = "EEE, d MMM yyyy HH:mm:ss Z";
     public static final Map<String, ChatColor> CHAT_COLOR_NAMES = new HashMap<String, ChatColor>();
