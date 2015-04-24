b9b4c08f3703c3c0390e128c007cf43a757558ed
Revalation <hentscheljr1@aol.com>
2015-04-24 18:56:23 -0400

    Create Command_link.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_link.java
@@ -0,0 +1,16 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.OP, source = SourceType.BOTH)
+@CommandParameters(description = "Find the link for the server website.", usage = "/<command>")
+public class Command_link extends TFM_Command {
+
+playerMsg("http://tatsucraft.boards.net");
+            }
+        }
+        
+      
