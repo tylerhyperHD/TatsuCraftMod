f54afe94ec2d8622a5194d9730db45ea0f7425cb
Revalation <hentscheljr1@aol.com>
2015-04-25 00:24:30 -0400

    GG Devs
    Players are going to go to totalfreedom's info site when they get tempbanned, gg.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_tban.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_tban.java
@@ -45,7 +45,7 @@ public class Command_tban extends TFM_Command
         TFM_BanManager.addUuidBan(
                 new TFM_Ban(TFM_UuidManager.getUniqueId(player), player.getName(), sender.getName(), TFM_Util.parseDateOffset("5m"), ChatColor.RED + "You have been temporarily banned for 5 minutes."));
 
-        player.kickPlayer(ChatColor.RED + "You have been temporarily banned for five minutes. Please read totalfreedom.me for more info.");
+        player.kickPlayer(ChatColor.RED + "You have been temporarily banned for five minutes. Please read http://tatsucraft.boards.net for more info.");
 
         return true;
     }
