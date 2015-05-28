6add5f2ec692ad30e41e0599fbe7d16cd3004d83
Scuph <hentscheljr1@aol.com>
2015-05-27 22:58:03 -0400

    Create Command_requesthelp.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_requesthelp.java
@@ -0,0 +1,23 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerData;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+@CommandParameters(description = "Request help", usage = "/<command>", aliases = "ineedhelp") {
+
+    @Override
+    public boolean run(CommandSender sender, Command cmd, String commandLabel, String[] args) 
+        TFM_Util.adminAction(sender.getName() + "Requesting help");
+        if (TFM_AdminList.isSuperAdmin())
+    {
+    player.sendMessage(sender.getName() + " wants help");
+    }
+        sender.sendMessage("You have requested help, a staff member will be with your shortly");
+        return true;
+    }
