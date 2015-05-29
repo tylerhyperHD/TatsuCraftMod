2bb4525c8c1972b3436e92f499502542d7a4cf0b
Drew <drew@finksdallas.com>
2015-05-29 12:23:45 -0500

    CHECK. YOUR. CODE.
    @Scuph, I'm tired of you committing extremely broken code that was very
    obviously not checked. I'm sick of correcting your obvious mistakes.
    Making your code in an IDE would be a good start.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_requesthelp.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_requesthelp.java
@@ -1,23 +1,24 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
-import me.StevenLawson.TotalFreedomMod.TFM_PlayerData;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
-import org.apache.commons.lang3.StringUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.ChatColor;
 
-@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
-@CommandParameters(description = "Request help", usage = "/<command>", aliases = "ineedhelp") {
-
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Request help", usage = "/<command>", aliases = "ineedhelp")
+public class Command_requesthelp extends TFM_Command
+{
     @Override
-    public boolean run(CommandSender sender, Command cmd, String commandLabel, String[] args) 
-        TFM_Util.adminAction(sender.getName() + "Requesting help");
-    if (TFM_AdminList.isSuperAdmin())
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-    player.sendMessage(sender.getName() + " wants help");
-    }
-        sender.sendMessage("You have requested help, a staff member will be with your shortly");
+        if (args.length == 0)
+        {
+            return false;
+        }
+
+        TFM_Util.adminAction(sender.getName(), "Requesting help", true);
+
         return true;
     }
+}
